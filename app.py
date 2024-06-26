d26e8027bc0b229d4dbc5fa55d9502d54c7ba060
Shreyas <shreyasgandhi673@gmail.com>
2024-06-26 21:07:28 +0530

    nav-bar done

--- app.py
+++ app.py
@@ -3,8 +3,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
